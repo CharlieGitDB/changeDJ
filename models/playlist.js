45a3a6f4e5b49135102e13ef96170285f33cd71c
CharlieGitDB <CharlieGitDB@users.noreply.github.com>
2016-02-20 03:21:29 -0600

    Update playlist.js

--- models/playlist.js
+++ models/playlist.js
@@ -1,13 +1,6 @@
 var mongoose = require('mongoose'),
     Schema = mongoose.Schema;
 
-// var PlaylistSchema = new Schema({
-//     userId: {type: String, required: true},
-//     songName: {type: String, required: true},
-//     songId: {type: String, required: true},
-//     imgUrl: {type: String, required: true},
-// });
-
 var PlaylistSchema = new Schema({
     userId: {type: String, required: true, index: { unique: true } },
     songs: [{songName: {type: String, required: true},
